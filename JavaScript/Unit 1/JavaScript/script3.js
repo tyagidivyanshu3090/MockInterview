24e8ee78c2a83ebf3bcfec940ef224e907eef4ef
Divyanshu Tyagi <tyagidivyanshu3090@gmail.com>
2025-07-01 18:23:56 +0530

    Hoisting

---
+++ JavaScript/Unit 1/JavaScript/script3.js
@@ -0,0 +1,7 @@
+//* Understanding the concept of var and global execution context
+//* Hoisting
+console.log("The value of age is", age);
+
+var age = 20;
+
+console.log("The value of age is " + age);
