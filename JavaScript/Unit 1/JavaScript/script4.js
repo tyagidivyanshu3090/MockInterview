c20fa19b22bf361bcd46fd7b3dadcd2835cf105c
Divyanshu Tyagi <tyagidivyanshu3090@gmail.com>
2025-07-01 19:13:24 +0530

    Adding the code 2

--- JavaScript/Unit 1/JavaScript/script4.js
+++ JavaScript/Unit 1/JavaScript/script4.js
@@ -1,13 +1,32 @@
-myFun(); // Line 1
+// ! Code 1
+// myFun(); // Line 1
 
-var myFun = function () {
-  console.log("first");
-}; // Line 2 (function expression)
+// var myFun = function () {
+//   console.log("first");
+// }; // Line 2 (function expression)
 
-myFun(); // Line 3
+// myFun(); // Line 3
 
-function myFun() {
-  console.log("second");
-} // Line 4 (function declaration)
+// function myFun() {
+//   console.log("second");
+// } // Line 4 (function declaration)
 
-myFun(); // Line 5
+// myFun(); // Line 5
+
+// ------------------------------------------------------------------
+
+// ! Code 2
+
+// var variable = 10;
+// (() => {
+//   console.log(variable);
+//   variable = 20;
+//   console.log(variable);
+// })();
+
+// console.log(variable);
+// var variable = 30;
+
+//* output -> 10 20 20
+
+// ----------------------------------------------
